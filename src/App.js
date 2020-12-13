2fd72e82f971100d8094747193ccd8934f6dd152
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-13 11:14:54 +0000

    added header, footer components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import Footer from './Components/Footer';
+import Header from './Components/Header';
+
 
 function App() {
-    return <Footer />
+    return <div>
+    <Header />
+    <Footer />
+    </div>
 }
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Header(){
+    return <header>
+        <h1>Hi! I am Matthew Berényi, and welcome to my site.</h1>
+        <p>I am a website and web application developer.</p>
+    </header>
+}
+
+export default Header;
