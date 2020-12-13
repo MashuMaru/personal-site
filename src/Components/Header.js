6706186cd49202c902553f88edcc79d13c2862b6
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-13 11:31:48 +0000

    added image (not showing correctly)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,6 +3,7 @@ import React from 'react';
 function Header(){
     return <header>
         <h1>Hi! I am Matthew Berényi, and welcome to my site.</h1>
+        <img src={require('./images/header-img.jpg')} alt="header-img"/>
         <p>I am a website and web application developer.</p>
     </header>
 }
