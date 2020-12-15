bdf220e65d3814a1f6b2770b8f566fe3e101c151
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-15 11:11:55 +0000

    CSS amendments

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 function Header(){
-    return <header>
+    return <header className='main-section'>
         <h1>Hi! I am Matthew Berényi, and welcome to my site.</h1>
-        <img src={require('./images/header-img.jpg')} alt="header-img"/>
+        {/* <img src={require('./images/header-img.jpg')} alt="header-img"/> */}
         <p>I am a website and web application developer.</p>
     </header>
 }
