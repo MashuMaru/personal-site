9223d62195b28ee02b9afc576e9bfc6afcfaf9ca
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-17 08:36:27 +0000

    Amendment to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ function Header(){
     return <header className='main-section'>
         <h1>Hi! I am Matthew Berényi, and welcome to my site.</h1>
         {/* <img src={require('./images/header-img.jpg')} alt="header-img"/> */}
-        <p>I am a website and web application developer.</p>
+        <p>Website and web application developer.</p>
     </header>
 }
 
