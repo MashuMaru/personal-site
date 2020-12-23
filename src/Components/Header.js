6637409deb967999aff9d8cd3305ebbe657087c3
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-23 16:23:45 +0000

    CSS changes to the <header />, added span

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header(){
     return <header className='main-section'>
-        <h1>Hi! I am Matthew Berényi, and welcome to my site.</h1>
+        <h1>Hello, I am <span className='name'>Matthew Berényi</span>, and welcome to my site.</h1>
         {/* <img src={require('./images/header-img.jpg')} alt="header-img"/> */}
         <p>Website and web application developer.</p>
     </header>
