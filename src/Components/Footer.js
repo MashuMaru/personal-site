17ec1cbacf8f45052fe9b7840e8a0ccc5579e762
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-24 10:01:15 +0000

    added button to <header /> (find out more - issues with loading images)

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,10 @@ var year = new Date().getFullYear();
 
 function Footer(){
     return <footer>
-    <p className='copyright'>© {year}. All Rights Reserved.</p>
-    <p className='copyright'>Website designed and developed by Mashu Maru.</p>
+    <p>© {year}. All Rights Reserved.</p>
+    <p>Website designed and developed by Mashu Maru.</p>
+    <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
     </footer>
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function Header(){
-    return <header className='main-section'>
-        <h1>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
-        {/* <img src={require('./images/header-img.jpg')} alt="header-img"/> */}
-        <p>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
-    </header>
+    return <header>
+            <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
+            {/* <img src={require('./images/header-img.jpg')} alt="header-img"/> */}
+            <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
+            <button className='find-button'>Find out more. <img src={require('./images/down-arrow.png')}/></button>
+            </header>
 }
 
 export default Header;
