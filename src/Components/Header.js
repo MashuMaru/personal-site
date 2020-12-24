f0840bc7131628a7ebb8fb623b4a8c1482ab7642
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-24 10:14:03 +0000

    removed image (unable to {require} images.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Header(){
     return <header>
-            <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
-            {/* <img src={require('./images/header-img.jpg')} alt="header-img"/> */}
-            <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
-            <button className='find-button'>Find out more. <img src={require('./images/down-arrow.png')}/></button>
-            </header>
+        <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
+        <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
+        <button className='find-button'>Find out more.</button>
+    </header>
+
 }
 
 export default Header;
+
+{/* <img src={require('./images/down-arrow.png')}  */}
