c9135f54062a829477fe5c8e06ea4e7a71d5fa85
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-24 12:31:37 +0000

    sorted image, re-routing required.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,7 +7,6 @@ function Footer(){
     <p>© {year}. All Rights Reserved.</p>
     <p>Website designed and developed by Mashu Maru.</p>
     <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-
     </footer>
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import HeaderImg from '../header-img.jpg';
 
 function Header(){
     return <header>
+    {/* <img className="header-img" src={HeaderImg} alt="header-img"></img> */}
         <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
         <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
         <button className='find-button'>Find out more.</button>
