caf9d0932b824c60ee00bae35c78aa02571b537d
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-29 09:41:30 +0000

    amendments to CSS and Header buttons

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,11 +7,13 @@ function About() {
     
     
         <h1 className="about-title">About</h1>
-        <img className="profile-pic" src={Mashumaru}></img>
+        <img className="profile-pic" src={Mashumaru} alt="pic-of-me"></img>
         <section id="about-description"></section>
         <h3 className="about-description">I am a self-taught website and web application developer.</h3>
         <p className="about-details">I started learning web development in 2019 online, and in my own time. 
-        Programming and development has always been an interest of mine.</p>
+        Programming and development has always been an interest of mine. </p>
+        <p className="about-details">Since then, I have built websites, webapplications and even a couple of web games.
+        I love coffee (especially Starbucks Caramel Frappuccino) and music. </p>
         
     </div>
     </section>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Arrow from '../down-arrow.png';
+// import Arrow from '../down-arrow.png';
 import HeaderImg from '../header-img1.jpg';
 
 function Header(){
@@ -7,8 +7,8 @@ function Header(){
     <img className="header-img" src={HeaderImg} alt="header-img"></img>
         <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
         <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
-        <button className='find-button' href="#about"><a href="#about">Find out more</a></button>
-        <button className='contact-button' href="#about"><a href="#about">Contact Me</a></button>
+        <button className='find-button' href="#about"><a href="#about">Find out more <i class="fas fa-angle-double-down"></i></a></button>
+        <button className='contact-button' href="#about"><a href="#about">Contact Me <i class="far fa-envelope"></i></a></button>
     </header>
 
 }
