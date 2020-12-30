2c2de5a09a5be74e78cea20d0faff4bb0d717ec5
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-30 11:01:26 +0000

    fixed div to 100vh (view port range dims)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,32 @@
-import React from 'react';
-// import Arrow from '../down-arrow.png';
+import React, { useState } from 'react';
 import HeaderImg from '../header-img1.jpg';
+
+// const [isMousedOver, setMousedOver] = useState(false);
+// const [isMousedOut, setMousedOut] = useState(true);
+
+// const image = <i className="far fa-envelope"></i>;
+
+// function mousedOver(){
+//         setMousedOver(true);
+//     }
+
+// function mousedOut() {
+//     setMousedOut(false);
+// }
+
+
 
 function Header(){
     return <header>
-    {/* <div className="header"> */}
+
     <img className="header-img" src={HeaderImg} alt="header-img"></img>
         <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
         <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
         <button className='find-button' href="#about"><a href="#about">Find out more <i class="fas fa-angle-double-down"></i></a></button>
-        <button className='contact-button' href="#about"><a href="#about">Contact Me <i class="far fa-envelope"></i></a></button>
-        {/* </div> */}
+        {/* <button onMouseOver={mousedOver} onMouseOut={mousedOut} className='contact-button' href="#about"> */}
+        <button className='contact-button' href="#about"><a href="#about">Contact Me <i className="far fa-envelope"></i> </a></button>
+        {/* <a href="#about">Contact Me {mousedOver ? <i className="far fa-envelope"></i> : null} {mousedOut && null}</a></button> */}
+
     </header>
 
 }
