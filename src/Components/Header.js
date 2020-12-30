a356a0c5aac1eb84b257d38442643c8b10d3e3f7
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2020-12-30 13:51:03 +0000

    added JS to contact button (onMouseOver, onMouseOut)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,25 @@
 import React, { useState } from 'react';
 import HeaderImg from '../header-img1.jpg';
 
-// const [isMousedOver, setMousedOver] = useState(false);
-// const [isMousedOut, setMousedOut] = useState(true);
-
-// const image = <i className="far fa-envelope"></i>;
-
-// function mousedOver(){
-//         setMousedOver(true);
-//     }
-
-// function mousedOut() {
-//     setMousedOut(false);
-// }
-
-
-
 function Header(){
+    const [isMousedOver, setMousedOver] = useState(false);
+    const [isMousedOut, setMousedOut] = useState(true);
+
+    function mousedOver(){
+        setMousedOver(true);
+    }
+
+    function mousedOut() {
+        setMousedOut(setMousedOver(false));
+    }
+
     return <header>
 
     <img className="header-img" src={HeaderImg} alt="header-img"></img>
         <h1 className='header-h1'>I am <span className='name'>Mashu Maru</span>, and welcome to my site.</h1>
         <p className='header-p'>A <span className='emphasis'>website</span> and <span className='emphasis'>web application</span> developer.</p>
         <button className='find-button' href="#about"><a href="#about">Find out more <i class="fas fa-angle-double-down"></i></a></button>
-        {/* <button onMouseOver={mousedOver} onMouseOut={mousedOut} className='contact-button' href="#about"> */}
-        <button className='contact-button' href="#about"><a href="#about">Contact Me <i className="far fa-envelope"></i> </a></button>
-        {/* <a href="#about">Contact Me {mousedOver ? <i className="far fa-envelope"></i> : null} {mousedOut && null}</a></button> */}
-
+        <button onMouseOver={mousedOver} onMouseOut={mousedOut} className='contact-button' href="#about"><a href="#about">{isMousedOver ? <i className="far fa-envelope"></i> : 'Contact Me'}</a></button>
     </header>
 
 }
