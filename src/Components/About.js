59caf5964ea4449a82f670c61ff95e837cdf882c
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-01-01 21:17:19 +0000

    CSS amendment to <about/> section (inline-block)

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,18 +4,20 @@ import Mashumaru from '../mashumaru-1.jpg'
 function About() {
 
     return <section id="about">
-    <div className='about-section'>
+    
     
     <hr></hr>
+    <div className='about-section'>
         <h1 className="about-title">About</h1>
 
         <img className="profile-pic" src={Mashumaru} alt="pic-of-me"></img>
-        {/* <section id="about-description"></section> */}
+        <div className="about-information">
         <h3 className="about-description">I am a self-taught website and web application developer.</h3>
         <p className="about-details">I started learning web development in 2019 using online courses and self-teaching in my own time. 
         Programming and coding has always been an interest of mine.</p>
         <p className="about-details">Since then, I have built websites, webapplications and even a couple of web games.
         I love coffee (especially Starbucks Caramel Frappuccino), hiking and music. </p>
+        </div>
         </div>
     </section>
 }
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 var year = new Date().getFullYear();
 
 function Footer(){
-    return <footer id="connect">
+    return  <div className="footer">
     <hr></hr>
     <p class="connect-p">For collaborations, project work or questions, contact me via the below links!</p>
     <a target="_blank" rel="noreferrer" href="https://www.instagram.com/mycodejournal/"><i class="fab fa-instagram fa-2x icons"></i></a>
@@ -14,7 +14,8 @@ function Footer(){
     <p>Website designed and developed by Mashu Maru.</p>
     {/* <p>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" rel="noreferrer" title="Pixel perfect">Pixel perfect</a> from <a target="_blank" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p> */}
     <p>Icons made by <a href="https://fontawesome.com/" rel="noreferrer">Font Awesome</a> from <a target="_blank" href="https://fontawesome.com/" rel="noreferrer" title="Font Awesome">www.fontawesome.com</a></p>
-    </footer>
+    </div>
+
 }
 
 export default Footer;
