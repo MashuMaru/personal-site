57051c465e3a503ce4f5fca11bdf29eefefbf6d6
MashuMaru <52470960+MashuMaru@users.noreply.github.com>
2021-02-10 05:43:38 +0000

    minor amendments to CSS and <About /> section

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,13 +21,13 @@ function About() {
         <h3 className="description">I am a self-taught website and web application developer.</h3>
         <p className="about-details">I started learning web development in 2019 using online courses and self-teaching in my own time. 
         Programming and coding has always been an interest of mine, so I decided to take the leap and change my career.</p>
-        <p className="about-details">Since then, I have built websites, web applications and even a couple of web games.
-        I love coffee (especially Starbucks Caramel Frappuccino), hiking, photography and music. </p>
+        <p className="about-details">Since then, I have built multiple websites, web applications and even a couple of web games.</p>
         <p className="about-details">My aim is to start my new career in Web Development within a company,
         that promotes personal and professional development with opportunities to work on new and existing websites and web applications.
         A place where I can look forward to a prosperous future! </p>
         <p className="about-details">I am constantly building new projects and learning new technologies to further enchance my skills. After all, 
         I love coding! </p>
+        <p className="about-details">I also love coffee (especially Starbucks Caramel Frappuccino), hiking, photography and making music.</p>
         <h3 className="description">Current skillset:</h3>
         <p className="about-details">HTML, CSS, Bootstrap, Materialize, JavaScript, jQuery, React.JS, Node.JS and currently learning PHP and SQL.</p>
         </div>
